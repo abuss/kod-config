c2132643be998eeac5f30fa0a8ea346c789a8f6a
Antal Buss <antal.buss@gmail.com>
2025-02-18 01:43:30 -0700

    Added initial host configurations

---
+++ hosts/eszkoz/development.lua
@@ -0,0 +1,30 @@
+-- Develop packages
+
+return {
+    -- Languages
+    "gcc",
+    "cmake",
+    "make",
+    "ninja",
+
+    "rustup",
+    
+    "lua",
+    
+    "go",
+    
+    "python",
+    "uv",
+    "ruff",
+    
+    -- Tools
+    "gitg",
+    "meld",
+    
+    -- Editors
+    -- "neovim",
+    "lite-xl",
+    "emacs-wayland",
+    "zed",
+
+}
